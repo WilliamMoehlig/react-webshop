21d36240ddabe1cf788d7b593952109527cb24b0
Karima <karima.elhachimi@euri.com>
2019-10-28 15:40:26 +0100

    rootReduced updated with cardProducts

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todoReducer';
 import notificationReducer from './notificationReducer';
+import productReducer from './productReducer';
 
 const rootReducer = combineReducers({
   todos: todoReducer,
   notifications: notificationReducer,
+  cartProducts: productReducer,
 });
 
 export default rootReducer;
