6f0272a5900b729a019e3e80ef3670125255d8fe
Jordy Rymenatns <jordy.rymenants@outlook.com>
2019-10-28 15:47:38 +0100

    Utility function return types

--- src/util/numberUtils.ts
+++ src/util/numberUtils.ts
@@ -1,8 +1,8 @@
-export function formatCurrency(number: number) {
+export function formatCurrency(number: number): string {
   return number.toFixed(2).replace('.', ',');
 }
 
-export function calcDiscount(base: number, discounted: number) {
+export function calcDiscount(base: number, discounted: number): number {
   const percentage = ((base - discounted) / base) * 100;
   return Math.round(percentage);
 }
