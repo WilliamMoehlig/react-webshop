d5937941e0690b87dffa3038a696212c820f17d1
karima-elhachimi <yuakarima@icloud.com>
2019-10-29 22:02:50 +0100

    product list item renders property product, tests pass

--- src/modules/checkout/components/ProductListItem.spec.tsx
+++ src/modules/checkout/components/ProductListItem.spec.tsx
@@ -1,28 +1,52 @@
 import React from 'react';
 
-import { render, within } from '@testing-library/react';
+import { render, within, getNodeText } from '@testing-library/react';
 import ProductListItem from './ProductListItem';
 
 import { getUtProduct } from '../../../../test/helper-objects';
 
 describe('ProductList item ', () => {
-  const renderComponent = (product = getUtProduct()) => {
+
+  const renderComponent = (product = getUtProduct(), quantity = 2) => {
     return {
-      ...render(<ProductListItem product={product} />),
+      ...render(<ProductListItem product={product} quantity={quantity} />),
     };
   };
+
   test('it renders', () => {
     const { container } = renderComponent();
+    
+    const tableRow = container.querySelector('tr');
 
-    expect(container.querySelector('tr')).toBeInTheDocument();
+    expect(tableRow).toBeInTheDocument();
   });
 
-  test('it has a product as property and renders image, title, sku, price, quantity', () => {
+  test('it has a product as property and renders image, title, sku, price', () => {
     const product = getUtProduct();
-    const { getByText, getByAltText, container } = renderComponent();
+    const {getByAltText, container } = renderComponent(product);
 
-    expect(getByAltText(product.title)).toBeInTheDocument();
-    expect(container.querySelector('h5').innerHTML).toBe(product.title);
-    //todo: continue HERE
+    const imageAlt = getByAltText(product.title);
+    const titleHeaderTag = container.querySelector('h5').innerHTML;
+    const skuSpan = container.getElementsByTagName('span')[0];
+    const priceSpan = container.getElementsByClassName('money')[0]
+
+    expect(imageAlt).toBeInTheDocument();
+    expect(titleHeaderTag).toBe(product.title);
+    expect(skuSpan.innerHTML).toBe(product.sku);
+    expect(priceSpan.innerHTML).toContain(product.price);
+  });
+
+  test('it gets quantity as a property and renders it', () => {
+    const product = getUtProduct();
+    const amountOfProduct = 3;
+    const { container} = renderComponent(product, amountOfProduct);
+
+    const quantity = container.getElementsByClassName('quantity')[0];
+
+    expect(quantity.innerHTML).toEqual(amountOfProduct.toString());
+  });
+
+  test('it deletes the product on button click', () => {
+
   });
 });
--- src/modules/checkout/components/ProductListItem.tsx
+++ src/modules/checkout/components/ProductListItem.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import  Product from '../../../models/Product'
 
-function ProductListItem({ product }) {
+interface ProductListItemProps {
+  product: Product,
+  quantity: number,
+}
+function ProductListItem({product, quantity}: ProductListItemProps) {
   return (
-    <tr>
+    <tr> 
       <th scope="row" className="border-0">
         <div className="p-2">
           <img
             src="https://dummyimage.com/300x300.jpg/5fa2dd/ffffff"
-            alt="aliquet"
+            alt={product.title}
             width="70"
             className="img-fluid rounded shadow-sm"
           />
           <div className="ml-3 d-inline-block align-middle">
-            <h5 className="mb-0">title</h5>
-            <span className="text-muted font-weight-normal font-italic d-block">sku</span>
+            <h5 className="mb-0">{product.title}</h5>
+            <span className="text-muted font-weight-normal font-italic d-block">{product.sku}</span>
           </div>
         </div>
       </th>
       <td className="border-0 align-middle">
         <strong>
-          <span className="money">€&nbsp;13,78</span>
+          <span className="money">€&nbsp;{(Math.round(product.price *100)/100).toFixed(2)}</span>
         </strong>
       </td>
       <td className="border-0 align-middle">
-        <strong>2</strong>
+        <strong className="quantity">{quantity}</strong>
       </td>
       <td className="border-0 align-middle">
         <button
