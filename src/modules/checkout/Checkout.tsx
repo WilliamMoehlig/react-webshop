28dbaec1e05d58978a43a3d7e6791908c8ea5e4a
Stijn Geens <stijn.geens@outlook.be>
2019-10-29 13:58:27 +0100

    Add checkout module

--- src/modules/checkout/Checkout.tsx
+++ src/modules/checkout/Checkout.tsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+
 function Checkout() {
-  return null;
+  return (
+    <div className="container-fluid">
+      <h2>Basket</h2>
+    </div>
+  );
 }
 
 export default Checkout;
