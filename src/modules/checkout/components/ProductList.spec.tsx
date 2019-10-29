db6dd5358e027e2346688ff7491aa5f992d96122
Karima <karima.elhachimi@euri.com>
2019-10-29 14:59:42 +0100

    table head test passes

--- src/modules/checkout/components/ProductList.spec.tsx
+++ src/modules/checkout/components/ProductList.spec.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import '@testing-library/dom';
 import { renderWithReduxRouter } from '../../../../test/render-utils';
 import ProductList from './ProductList';
+import { getByText } from '@testing-library/dom';
 
 describe('ProductList', () => {
   const renderComponent = () => {
+    const result = renderWithReduxRouter(<ProductList />);
     return {
-      ...renderWithReduxRouter(<ProductList />),
+      ...result,
+      getTableHeader: () => result.container.querySelector('thead'),
     };
   };
 
@@ -15,5 +19,16 @@ describe('ProductList', () => {
     const productList = getByRole('table');
 
     expect(productList).toBeInTheDocument();
+  });
+
+  test('table head has product, price, quantity and remove', () => {
+    const { getTableHeader } = renderComponent();
+
+    const productTableHeader = getTableHeader();
+
+    expect(getByText(productTableHeader, /product/i)).toBeInTheDocument();
+    expect(getByText(productTableHeader, /price/i)).toBeInTheDocument();
+    expect(getByText(productTableHeader, /quantity/i)).toBeInTheDocument();
+    expect(getByText(productTableHeader, /remove/i)).toBeInTheDocument();
   });
 });
---
+++ src/modules/checkout/components/ProductListItem.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ProductListItem from './ProductListItem';
+
+describe('ProductList item ', () => {
+  test('it renders', () => {
+    const { container } = render(<ProductListItem />);
+    expect(container.querySelector('tr')).toBeInTheDocument();
+  });
+});
