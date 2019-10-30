64c03f1232e4f300cd2fdcce50d38b49c511d7c9
Karima <karima.elhachimi@euri.com>
2019-10-30 08:26:36 +0100

    productListItem tests pass

--- src/modules/checkout/components/ProductListItem.spec.tsx
+++ src/modules/checkout/components/ProductListItem.spec.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { render, within, getNodeText } from '@testing-library/react';
 import ProductListItem from './ProductListItem';
-
 import { getUtProduct } from '../../../../test/helper-objects';
 
 describe('ProductList item ', () => {
-
-  const renderComponent = (product = getUtProduct(), quantity = 2) => {
+  const renderComponent = (product = getUtProduct(), quantity = 2, onButtonClick = () => {}) => {
     return {
-      ...render(<ProductListItem product={product} quantity={quantity} />),
+      ...render(<ProductListItem product={product} quantity={quantity} onButtonClicked={onButtonClick} />),
     };
   };
 
   test('it renders', () => {
     const { container } = renderComponent();
-    
+
     const tableRow = container.querySelector('tr');
 
     expect(tableRow).toBeInTheDocument();
@@ -23,14 +21,15 @@ describe('ProductList item ', () => {
 
   test('it has a product as property and renders image, title, sku, price', () => {
     const product = getUtProduct();
-    const {getByAltText, container } = renderComponent(product);
+    const { getByAltText, container } = renderComponent(product);
 
     const imageAlt = getByAltText(product.title);
     const titleHeaderTag = container.querySelector('h5').innerHTML;
     const skuSpan = container.getElementsByTagName('span')[0];
-    const priceSpan = container.getElementsByClassName('money')[0]
+    const priceSpan = container.getElementsByClassName('money')[0];
 
     expect(imageAlt).toBeInTheDocument();
+    expect(imageAlt.getAttribute('src')).toBe(product.image);
     expect(titleHeaderTag).toBe(product.title);
     expect(skuSpan.innerHTML).toBe(product.sku);
     expect(priceSpan.innerHTML).toContain(product.price);
@@ -39,14 +38,24 @@ describe('ProductList item ', () => {
   test('it gets quantity as a property and renders it', () => {
     const product = getUtProduct();
     const amountOfProduct = 3;
-    const { container} = renderComponent(product, amountOfProduct);
+    const { container } = renderComponent(product, amountOfProduct);
 
     const quantity = container.getElementsByClassName('quantity')[0];
 
     expect(quantity.innerHTML).toEqual(amountOfProduct.toString());
   });
 
-  test('it deletes the product on button click', () => {
+  test('it calls the delete function on button click that has an icon', () => {
+    const mockFunction = jest.fn();
+    const product = getUtProduct();
+    const { getByLabelText, container } = renderComponent(product, 2, mockFunction);
 
+    const button = getByLabelText(/remove/i);
+    const iTags = container.getElementsByTagName('i');
+    const icon = Object.values(iTags).find(t => t.className === 'fa fa-trash');
+
+    fireEvent.click(button);
+    expect(mockFunction).toBeCalled();
+    expect(icon).toBeInTheDocument();
   });
 });
--- src/modules/checkout/components/ProductListItem.tsx
+++ src/modules/checkout/components/ProductListItem.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import  Product from '../../../models/Product'
+import Product from '../../../models/Product';
 
 interface ProductListItemProps {
-  product: Product,
-  quantity: number,
+  product: Product;
+  quantity: number;
+  onButtonClicked: () => void;
 }
-function ProductListItem({product, quantity}: ProductListItemProps) {
+function ProductListItem({ product, quantity, onButtonClicked }: ProductListItemProps) {
   return (
-    <tr> 
+    <tr>
       <th scope="row" className="border-0">
         <div className="p-2">
-          <img
-            src="https://dummyimage.com/300x300.jpg/5fa2dd/ffffff"
-            alt={product.title}
-            width="70"
-            className="img-fluid rounded shadow-sm"
-          />
+          <img src={product.image} alt={product.title} width="70" className="img-fluid rounded shadow-sm" />
           <div className="ml-3 d-inline-block align-middle">
             <h5 className="mb-0">{product.title}</h5>
             <span className="text-muted font-weight-normal font-italic d-block">{product.sku}</span>
@@ -24,7 +20,7 @@ function ProductListItem({product, quantity}: ProductListItemProps) {
       </th>
       <td className="border-0 align-middle">
         <strong>
-          <span className="money">€&nbsp;{(Math.round(product.price *100)/100).toFixed(2)}</span>
+          <span className="money">€&nbsp;{(Math.round(product.price * 100) / 100).toFixed(2)}</span>
         </strong>
       </td>
       <td className="border-0 align-middle">
@@ -36,6 +32,7 @@ function ProductListItem({product, quantity}: ProductListItemProps) {
           className="btn btn-link btn-sm p-0 text-dark"
           aria-label="Remove"
           style={{ verticalAlign: 'top' }}
+          onClick={onButtonClicked}
         >
           <i className="fa fa-trash" />
         </button>
