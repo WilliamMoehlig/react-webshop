259c2a2219dead7f237522a5772c1d2127d60050
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-10-30 12:39:13 +0100

    Checkout module

--- src/modules/checkout/Checkout.spec.tsx
+++ src/modules/checkout/Checkout.spec.tsx
@@ -3,13 +3,29 @@ import { render } from '@testing-library/react';
 
 import Checkout from './Checkout';
 
+jest.mock('./components/ProductList', () => () => {
+  return <div data-testid="product-list-component" />;
+});
+
+jest.mock('./components/Instructions', () => () => {
+  return <div data-testid="instructions-component" />;
+});
+
+jest.mock('./components/Overview', () => () => {
+  return <div data-testid="overview-component" />;
+});
+
 describe('Checkout module', () => {
   test('it renders', () => {
-    const { getByRole } = render(<Checkout />);
+    const { getByRole, getByTestId } = render(<Checkout />);
 
     const header = getByRole('heading');
 
     expect(header).toBeInTheDocument();
     expect(header).toHaveTextContent(/basket/i);
+
+    getByTestId('product-list-component');
+    getByTestId('instructions-component');
+    getByTestId('overview-component');
   });
 });
--- src/modules/checkout/Checkout.tsx
+++ src/modules/checkout/Checkout.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+
 import ProductList from './components/ProductList';
+import Instructions from './components/Instructions';
+import Overview from './components/Overview';
 
 function Checkout() {
   return (
     <div className="container-fluid">
       <h2>Basket</h2>
+      <div className="row">
+        <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+          <ProductList />
+        </div>
+      </div>
+      <div className="row py-5 p-4 bg-white rounded shadow-sm">
+        <div className="col-lg-6">
+          <Instructions />
+        </div>
+        <div className="col-lg-6">
+          <Overview />
+        </div>
+      </div>
     </div>
   );
 }
